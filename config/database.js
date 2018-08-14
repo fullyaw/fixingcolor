33063920ea741f645857427a6af1e7dcc669fde4
fullyaw <42380980+fullyaw@users.noreply.github.com>
2018-08-14 10:43:23 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,7 @@
+var user = 'fixingcolor';
+var password = 'test';
+
 module.exports = {
   'secret':'0199089ColorNinja',
-  'database': 'mongodb://localhost/ColorNinja'
+  'database': 'mongodb://' +user+ ':' +password+ '@ds121382.mlab.com:21382/fixingcolor'
 };
