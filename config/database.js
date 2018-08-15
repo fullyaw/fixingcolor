27f6c6a1449307279616541e572f05267ac317a9
fullyaw <42380980+fullyaw@users.noreply.github.com>
2018-08-15 03:05:01 -0400

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
-var user = 'www';
-var password = 'testing123';
+var user = '';
+var password = '';
 
 module.exports = {
   'secret':'0199089ColorNinja',
-  'database': 'mongodb://' +user+ ':' +password+ '@ds121382.mlab.com:21382/fixingcolor'
+  'database': 'mongodb://' +user+ ':' +password+ '@ds121382.mlab.com:21382/fixingcolor',
+  'email_to': 'fullyaw.info@gmail.com',
+  'email_pwd': 'test',
+  'email_subject': 'FixingColor WWW Inquiry'
 };
