23ccd9eacc3613c68b0b541b70b2de31ad27ef1a
fullyaw <42380980+fullyaw@users.noreply.github.com>
2018-08-15 12:35:44 -0400

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 var user = process.env.mongoose_user;
 var password = process.env.mongoose_pwd;
 
+console.log('Found Mongoose:' + user + ' ' + password);
+
 module.exports = {
   'secret':'0199089ColorNinja',
   'database': 'mongodb://' +user+ ':' +password+ '@ds121382.mlab.com:21382/fixingcolor',
