c49b00cee26eaa136169fb6cd205157abf70c636
fullyaw <42380980+fullyaw@users.noreply.github.com>
2018-08-15 12:29:19 -0400

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
-var user = 'www';
-var password = 'testing123';
+var user = process.env.mongoose_user;
+var password = process.env.mongoose_pwd;
 
 module.exports = {
   'secret':'0199089ColorNinja',
   'database': 'mongodb://' +user+ ':' +password+ '@ds121382.mlab.com:21382/fixingcolor',
   'email_to': 'fixingcolor@gmail.com',
-  'email_pwd': 'test',
   'email_subject': 'FixingColor WWW Inquiry'
 };
