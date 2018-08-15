e62d6d1b31cf636a1337d09b98f27c61dde9db05
fullyaw <42380980+fullyaw@users.noreply.github.com>
2018-08-15 03:52:28 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
-var user = '';
-var password = '';
+var user = 'www';
+var password = 'testing123';
 
 module.exports = {
   'secret':'0199089ColorNinja',
   'database': 'mongodb://' +user+ ':' +password+ '@ds121382.mlab.com:21382/fixingcolor',
-  'email_to': 'fullyaw.info@gmail.com',
+  'email_to': 'fixingcolor@gmail.com',
   'email_pwd': 'test',
   'email_subject': 'FixingColor WWW Inquiry'
 };
